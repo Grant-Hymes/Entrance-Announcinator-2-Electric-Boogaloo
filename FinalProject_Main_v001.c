ca86de49096a47392636d1694eeb808185d57af4
Grant <gahymes@gmail.com>
2023-04-12 11:51:51 -0500

    Update FinalProject_Main_v001.c

--- FinalProject_Main_v001.c
+++ FinalProject_Main_v001.c
@@ -5,8 +5,70 @@
  * Created on April 12, 2023, 11:19 AM
  */
 
+void setup(void);
+int main(void);
+void writeColor(int r, int g, int b);
+
+// CW1: FLASH CONFIGURATION WORD 1 (see PIC24 Family Reference Manual 24.1)
+#pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
+#pragma config FWDTEN = OFF        // Watchdog Timer Enable (Watchdog Timer is disabled)
+#pragma config GWRP = OFF          // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF           // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF        // JTAG Port Enable (JTAG port is disabled)
+
+
+// CW2: FLASH CONFIGURATION WORD 2 (see PIC24 Family Reference Manual 24.1)
+#pragma config I2C1SEL = PRI       // I2C1 Pin Location Select (Use default SCL1/SDA1 pins)
+#pragma config IOL1WAY = OFF       // IOLOCK Protection (IOLOCK may be changed via unlocking seq)
+#pragma config OSCIOFNC = ON       // Primary Oscillator I/O Function (CLKO/RC15 functions as I/O pin)
+#pragma config FCKSM = CSECME      // Clock Switching and Monitor (Clock switching is enabled, 
+                                       // Fail-Safe Clock Monitor is enabled)
+#pragma config FNOSC = FRCPLL      // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
 
 #include "xc.h"
+
+void setup(void)
+{
+    // execute once code goes here
+    CLKDIVbits.RCDIV = 0;  //Set RCDIV=1:1 (default 2:1) 32MHz or FCY/2=16M
+    AD1PCFG = 0x9fff;            //sets all pins to digital I/O
+    TRISA = 0b1111111111111110;  //Set pin RA0 to output
+    LATA = 0x0001;               //Set pin RA0 high
+}
+
+void writeColor(int r, int g, int b) { 
+    
+    // writes red color
+    for (int i = 128; i > 0; i = i>>1) {
+        if (r&i) {
+            write_1();
+        } else {
+            write_0();
+        }
+    }
+    
+    // writes green color
+    for (int i = 128; i > 0; i = i>>1) {
+        if (g&i) {
+            write_1();
+        } else {
+            write_0();
+        }
+    }
+    
+    // writes blue color
+    for (int i = 128; i > 0; i = i>>1) {
+        if (b&i) {
+            write_1();
+        } else {
+            write_0();
+        }
+    }
+        
+    // allows for reset to work     
+    delay_100us();
+        
+}
 
 int main(void) {
     return 0;
