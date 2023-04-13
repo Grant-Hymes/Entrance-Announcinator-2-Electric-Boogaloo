0f9ae97dfb12c67f3929962a492e5a4fc60c86b1
carra166 <carra166@umn.edu>
2023-04-13 14:24:14 -0500

    Added formal library setup for the music playing

---
+++ FinalProject_MusicLibrary.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   FinalProject_MusicLibrary.h
+ * Author: egcar
+ *
+ * Created on April 13, 2023, 1:46 PM
+ */
+
+#ifndef FINALPROJECT_MUSICLIBRARY_H
+#define	FINALPROJECT_MUSICLIBRARY_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FINALPROJECT_MUSICLIBRARY_H */
+
---
+++ FinalProject_MusicLibrary_v001.c
@@ -0,0 +1,13 @@
+/*
+ * File:   FinalProject_MusicLibrary_v001.c
+ * Author: egcar
+ *
+ * Created on April 13, 2023, 1:46 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
